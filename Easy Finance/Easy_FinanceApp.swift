b0eebfa473536fce2cbed28575bac497e7cc984b
Shukhrat Nurov <shuhratnurov32@gmail.com>
2023-06-25 15:27:06 +0500

    Initial Commit

---
+++ Easy Finance/Easy_FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_FinanceApp.swift
+//  Easy Finance
+//
+//  Created by Shuhrat Nurov on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Easy_FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
